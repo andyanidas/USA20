339ed0139124310002926141623f4cd9de02ade9
Lkhagva-erdene <anidas.com@gmail.com>
2021-03-16 05:36:05 +0300

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	//declare 
+	int x = 15;		//integer buhel too
+	int y = 20;
+	int sum;
+	sum = x + y; // = ->> assignment sign
+	x = x + 20;
+	
+	
+	
+	cout<<x + y;
+
+	
+	
+	
+	return 0;
+}
---
+++ 4.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	int x, y;
+	
+	cout<<"Enter x : "<<endl;
+	cin>>x;
+	cout<<"Enter y : ";
+	cin>>y;
+	
+	cout<<"x+y="<<x+y;
+	
+	
+	
+	return 0;
+}
